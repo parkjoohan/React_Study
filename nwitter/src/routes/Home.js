3c82093caefdf09c5bcec4b5881c67782803c76e
alex0317@naver.com <joohanpark1513@gmail.com>
2022-01-30 12:23:01 +0900

    0130 공부

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,28 +1,41 @@
-import React, {useState} from 'react';
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
+import { addDoc, collection } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
-
-    const onSubmit = (event) => {
-        event.preventDefault();
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService, "nweets"), {
+                nweet,
+                createdAt: Date.now(),
+            });
+        console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    setNweet("");
     };
-    const onChange = (event) => {
-        const { target: { value } } = event;
+
+    const onChange = ({ target: { value } }) => {
         setNweet(value);
     };
 
     return (
         <div>
-            <from onSubmit={onSubmit}>
+            <form onSubmit={onSubmit}>
                 <input
-                    value={nweet}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="What's on your mind"
-                    maxLength={120} />
-                <input type="submit" value="Nweet"/>
-            </from>
+                value={nweet}
+                onChange={onChange}
+                type="text"
+                placeholder="What's on your mind?"
+                maxLength={120}
+                />
+                <input type="submit" value="Nweet" />
+            </form>
         </div>
     );
-}
+};
+
 export default Home;
