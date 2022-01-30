42c2e84c460e225cda2702b76b8d72d3789efc45
alex0317@naver.com <joohanpark1513@gmail.com>
2022-01-30 12:37:48 +0900

    0130 공부

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,9 +1,25 @@
 import { dbService } from 'fbase';
-import React, { useState } from 'react';
-import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async () => {
+        const dbNweets = await getDocs(collection(dbService, "nweets"));
+        
+        dbNweets.forEach((document) => {
+            const nweetObject = { ...document.data(), id: document.id };
+            setNweets((prev) => [nweetObject, ...prev]);
+            console.log(document.data());
+        });
+    };
+        
+    useEffect(() => {
+        getNweets();
+    }, []);
+
     const onSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -34,6 +50,13 @@ const Home = () => {
                 />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map(nweet =>
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
