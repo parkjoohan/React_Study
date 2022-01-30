f2bb0841f39a5d446026174297c45d40f2ee8bde
alex0317@naver.com <joohanpark1513@gmail.com>
2022-01-30 12:13:41 +0900

    0130 공부

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import React, { useState } from "react";
+import {HashRouter as Router, Route, Routes,} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
-const AppRouter = ({isLoggedIn}) => {
+const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
-            <Routes>
-                {isLoggedIn ? (
-                    <>
-                        <Route exact path="/" element={<Home />}>
-                        </Route>
-                    </>
-                ) : (
-                    <Route exact path="/"  element={<Auth />}>
-                    </Route>
-                )}
-            </Routes>
+        {isLoggedIn && <Navigation />}
+        <Route>
+            {isLoggedIn ? (
+            <>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/profile">
+                    <Profile />
+                </Route>
+            </>
+            ) : (
+            
+                <Route exact path="/">
+                <Auth />
+                </Route>
+            
+            )}
+        </Route>
         </Router>
     );
 };
-
 export default AppRouter;
--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,3 +1,17 @@
-import React from 'react';
-
-export default () => <span>Profile</span>;
+import { authService } from "fbase";
+import React from "react";
+import { useHistory } from "react-router";
+const Profile = (props) => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  console.log(props);
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+export default Profile;
