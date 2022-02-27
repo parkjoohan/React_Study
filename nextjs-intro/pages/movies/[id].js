09406f32059cc035688cdc51050ee6a7956a5f23
alex0317@naver.com <joohanpark1513@gmail.com>
2022-02-27 19:32:09 +0900

    Next.Js Router Detail

--- nextjs-intro/pages/movies/[id].js
+++ nextjs-intro/pages/movies/[id].js
@@ -1,7 +1,10 @@
 import { useRouter } from "next/router";
 
 export default function Detail() {
-  const router = useRouter();
-  console.log(router);
-  return "detail";
+    const router = useRouter();
+    return (
+        <div>
+            <h4>{router.query.title || "Loading..."}</h4>
+        </div>
+    );
 }
