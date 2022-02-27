249fcf941531920226b66e7f4e9868a4e6394d2d
alex0317@naver.com <joohanpark1513@gmail.com>
2022-02-27 17:56:20 +0900

    Next.Js fetching data

--- nextjs-intro/pages/_app.js
+++ nextjs-intro/pages/_app.js
@@ -1,16 +1,10 @@
-import NavBar from '../components/NavBar';
-import '../styles/globals.css';
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
-export default function App({Component, pageProps}) {
+export default function MyApp({ Component, pageProps }) {
     return (
-        <>
-            <NavBar />
+        <Layout>
             <Component {...pageProps} />
-            <style jsx global>{`
-                a {
-                    color: white;
-                }
-            `}</style>
-        </>
-    )
+        </Layout>
+    );
 }
