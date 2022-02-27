af5d560d047a5d83b9b8dea4151996bd8e56c174
alex0317@naver.com <joohanpark1513@gmail.com>
2022-02-27 15:29:47 +0900

    Next.Js Styles JSX _app.js

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -3,8 +3,7 @@ import NavBar from '../components/NavBar';
 export default function Home() {
     return (
         <div>
-            <NavBar />
-            <h1>Hello</h1>
+            <h1 className=''>Hello</h1>
         </div>
     )
 }
