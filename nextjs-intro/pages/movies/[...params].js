02873350423d483f536c47ac5b46634dc162d67c
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-01 16:32:31 +0900

    Next.Js Finish

--- nextjs-intro/pages/movies/[...params].js
+++ nextjs-intro/pages/movies/[...params].js
@@ -2,11 +2,10 @@ import Seo from "../../components/Seo";
 import { useRouter } from "next/router";
 
 export default function Detail({ params }) {
-    const router = useRouter();
     const [title, id] = params || [];
     return (
         <div>
-            <Seo title={title} />
+            <Seo title = {title} />
             <h4>{title}</h4>
         </div>
     );
