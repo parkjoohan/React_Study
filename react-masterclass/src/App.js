2ea91b2e33de5f20666e7c2977b745cb75be0db3
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-03 19:05:46 +0900

    리액트 복습 - styled-components & animations

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,27 +1,50 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
 `
 
-const Input = styled.input.attrs({required: true, minLength: 10})`
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 2s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0      ;
+    }
+  }
+`
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😋</span>
+      </Box>
+    </Wrapper>
   )
 }
 
