95e8d5c69e315bce6d671d970032f2ba5da8fe78
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-05 11:20:29 +0900

    리액트 복습 - Data

--- react-masterclass/src/routes/Coin.tsx
+++ react-masterclass/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router";
 import styled from "styled-components";
 
@@ -37,7 +37,21 @@ function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams<RouteParams>();
     const { state } = useLocation<RouteState>();
-    console.log(state.name)
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState({});
+    
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json(); 
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, [])
 
     return (
         <Container>
