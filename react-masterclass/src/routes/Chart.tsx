6f8bb0cd231499ec3e94151ad7d5b71356df21c0
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-06 22:47:55 +0900

    리액트 복습 - chart(2)

--- react-masterclass/src/routes/Chart.tsx
+++ react-masterclass/src/routes/Chart.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
     coinId: string;
@@ -20,6 +21,52 @@ function Chart({ coinId }: ChartProps) {
     const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId), {
         refetchInterval: 10000,
     });
-    return <h1>Chart</h1>;
+    return (
+        <div>
+            {isLoading ? (
+                "Loading chart..."
+            ) : (
+                    <ApexChart
+                        type="line"
+                        series={[
+                            {
+                                name: "price",
+                                data: data?.map(price => price.close) as number[]
+                            }
+                        ]}
+                        options={{
+                            theme: {
+                                mode: "dark"
+                            },
+                            chart: {
+                                height: 300,
+                                width: 500,
+                                toolbar: {
+                                    show: false,
+                                },
+                                background: "transparent",
+                            },
+                            grid: {
+                                show: false,
+                            },
+                            xaxis: {
+                                axisBorder: { show: false },
+                                axisTicks: { show: false },
+                                labels: { show: false },
+                            },
+                            yaxis: {
+                                labels: {
+                                    show: false,
+                                }
+                            },
+                            stroke: {
+                                curve: "smooth",
+                                width: 4,
+                            }
+                    }}
+                />
+            )}
+        </div>
+    )
 }
 export default Chart;
