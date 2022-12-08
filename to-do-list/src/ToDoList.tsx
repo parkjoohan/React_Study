c6ca22182a27809e8a9ccfd280b4c6197058a5d1
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-08 21:59:33 +0900

    리액트 복습 - react-hook-form & Form(3)

--- to-do-list/src/ToDoList.tsx
+++ to-do-list/src/ToDoList.tsx
@@ -29,21 +29,78 @@ import { useForm } from "react-hook-form";
 //     )
 // }
 
+interface IFrom {
+    email: string,
+    firstName: string,
+    lastName: string,
+    username: string,
+    password: string,
+    password1: string,
+}
+
 function ToDoList() {
-    const { register, handleSubmit, formState } = useForm();
+    const { register, handleSubmit, formState:{errors} } = useForm<IFrom>({defaultValues: {email: "@naver.com",}});
     const onValid = (data: any) => {
         console.log(data);
     }
-    console.log(formState.errors)
     return (
         <div>
-            <form style={{display: "flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
-                <input {...register("email", { required: true })} placeholder="Email" />
-                <input {...register("firstName", { required: true })} placeholder="First Name" />
-                <input {...register("lastName", { required: true })} placeholder="Last Name" />
-                <input {...register("username", { required: true, minLength: 10 })} placeholder="Username" />
-                <input {...register("password", { required: true, minLength: 10 })} placeholder="Password" />
-                <input {...register("password1", { required: "Password is required", minLength: {value: 10, message: "Your password is too short."} })} placeholder="Password1" />
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={handleSubmit(onValid)}
+            >
+                <input
+                    {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                            message: "Only naver.com emails allowed"
+                        }
+                    })}
+                    placeholder="Email"
+                />
+                <span>{errors?.email?.message as string}</span>
+                <input
+                    {...register("firstName", {
+                        required: "firstName is required"
+                    })}
+                    placeholder="First Name"
+                />
+                <span>{errors?.firstName?.message as string}</span>
+                <input
+                    {...register("lastName", {
+                        required: "lastName is required"
+                    })}
+                    placeholder="Last Name"
+                />
+                <span>{errors?.lastName?.message as string}</span>
+                <input
+                    {...register("username", {
+                        required: "username is required",
+                        minLength: 10
+                    })}
+                    placeholder="Username"
+                />
+                <span>{errors?.username?.message as string}</span>
+                <input
+                    {...register("password", {
+                        required: "password is required",
+                        minLength: 10
+                    })}
+                    placeholder="Password"
+                />
+                <span>{errors?.password?.message as string}</span>
+                <input
+                    {...register("password1", {
+                        required: "password1 is required",
+                        minLength: {
+                            value: 10,
+                            message: "Your password is too short."
+                        }
+                    })}
+                    placeholder="Password1"
+                />
+                <span>{errors?.password1?.message as string}</span>
                 <button>Add</button>
             </form>
         </div>
