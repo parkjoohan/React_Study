31cac615d7e95c162abd04f983212aede420f7d8
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-25 09:56:46 +0900

    리액트 복습 - Basic Animations

--- framer-motion/src/index.tsx
+++ framer-motion/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
-import App from "./App";
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import App from './App';
 import { darkTheme } from "./theme";
-import { createGlobalStyle } from "styled-components";
+
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -61,24 +61,24 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
+  background:linear-gradient(135deg,#e09,#d0e);
   color:black;
   line-height: 1.2;
-  background:linear-gradient(135deg,#e09,#d0e);
-  
 }
 a {
   text-decoration:none;
   color:inherit;
 }
 `;
-ReactDOM.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
 );
