5c6053b9717553f391f6787834f3bb689e7b2f31
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-30 00:42:22 +0900

    리액트 복습 - Variants CSS(4)

--- framer-motion/src/App.tsx
+++ framer-motion/src/App.tsx
@@ -19,13 +19,14 @@ const Box = styled(motion.div)`
 
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
-  click: {scale: 1, borderRadius: "100px"}
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46,204,113)", transition: {duation: 10} },
 }
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+      <Box drag variants={boxVariants} whileHover="hover" whileDrag="drag" whileTap="click" />
     </Wrapper>
   );
 }
